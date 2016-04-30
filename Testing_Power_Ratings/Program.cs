8a3c68a63ca051b793b8409b98f05f804aafd302
JonathanSpalding <Jon.Spalding@hotmail.com>
2016-04-30 10:38:08 -0600

    CS 1400 C#

---
+++ Testing_Power_Ratings/Program.cs
@@ -0,0 +1,67 @@
+﻿// Author: Jonathan Spalding
+// Assignment: Project 11
+// Instructor "Roger deBry
+// Clas: CS 1400
+// Date Written: 4/16/2016
+//
+// "I declare that the following source code was written solely by me. I understand that copying any source code, in whole or in part, constitutes cheating, and that I will receive a zero on this project if I am found in violation of this policy."
+//----------------------------------------------------
+using System;
+using static System.Console;
+using System.IO;
+
+namespace Testing_Power_Ratings
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            // state all constants and variables
+            int num = 0;
+            double count = 0;
+            string inputString;
+            const double MAX = 50; 
+            // create an object for Test Resistors.
+            TestResistors objectRef = new TestResistors();
+            //Write Instructions
+            WriteLine("Resistor Batch Test Analysis Program\nData file must be in your Documents folder\nPlease enter the file name: ");
+            // crete a string event to run C# agiin.
+            string environment = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal) + "\\";
+            //create variable for the file with "theTextFile"
+            StreamReader theTextFile = new StreamReader(environment + ReadLine());
+            // put the first line in the file in an array, and split it.
+            string[] a1 = theTextFile.ReadLine().Split();
+            // first element in the array parsed to a double and assigned to its own variable
+            double resistance = double.Parse(a1[0]);
+            // call back the resistance 
+            objectRef.SetResistance(resistance);
+            // set the power in the array to a double variable.
+            double power = double.Parse(a1[1]);
+            // call back the Set Power Rating
+            objectRef.SetPowerRating(power);
+            // do while loop for when the file has more that can be read, or it is less than 50.
+            do
+            {
+                // set the input string variable equall with ReadLine
+                inputString = theTextFile.ReadLine();
+                // if there is data to be read
+                if (inputString != null)
+                    // find the power of that line of data according to Ohm's Law
+                    objectRef.OhmsLaw(double.Parse(inputString));
+            }
+            // if there isn't any data, or if it reaches 50 items, exit the do-while loop
+            while (inputString != null && num < MAX);
+            // find out how many items have been read in the file
+            count = objectRef.GetCount();
+            // WriteLine a title for the output
+            WriteLine("\n\nRes#\tDissipation\tPassed");
+            // for loop that runs through as many times as there were items in the file
+            for (var i = 0; i < count; ++i)
+                // Write the totals according to the format of the title.
+                WriteLine($"{(i + 1)}\t{objectRef.GetPower(i):f}\t\t{objectRef.Result(i)}");
+            // ReadKey for keeping the window open.
+            ReadKey(true);
+        }
+    }
+}
---
+++ Testing_Power_Ratings/TestResistors.cs
@@ -0,0 +1,106 @@
+﻿// Author: Jonathan Spalding
+// Assignment: Project 11
+// Instructor "Roger deBry
+// Clas: CS 1400
+// Date Written: 4/16/2016
+//
+// "I declare that the following source code was written solely by me. I understand that copying any source code, in whole or in part, constitutes cheating, and that I will receive a zero on this project if I am found in violation of this policy."
+//----------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing_Power_Ratings
+{
+    class TestResistors
+    {
+        // state constants and all variables
+        private const int MAX = 50;
+        private double resistance;
+        private double powerRating;
+        private int count;
+        // create two arrays to keep together the voldate, and power
+        private double[] voltageArray;
+        private double[] powerArray;
+        // The default constructor
+        // Purpose: It sets all declared variables (data members of the class) to what they need to be.
+        // Parameters:
+        // Returns: None
+        public TestResistors()
+        {
+            voltageArray = new double[MAX];
+            powerArray = new double[MAX];
+            count = 0;
+        }
+        // The SetResistance method
+        // Purpose: creates a variable for resistance
+        // Parameters: double Resistance
+        // Returns: none
+        public void SetResistance(double Resistance)
+        {
+            resistance = Resistance;
+        }
+        // The SetPowerRating method
+        // Purpose: creates a variable for Power
+        // Parameters: double power
+        // Returns: none
+        public void SetPowerRating(double power)
+        {
+            powerRating = power;
+        }
+        // The StoreVoltageValue method
+        // Purpose: Gets the voltage, and divides it by the resistance to find the power.
+        // Parameters: double double
+        // Returns: none
+        public void OhmsLaw(double voltage)
+        {
+            voltageArray[count] = voltage;
+            // finds the power according to Ohm's Law and assigns it to the powerArray
+            powerArray[count] = Math.Pow(voltage, 2) / resistance;
+            // incrament count
+            ++count;
+        }
+        // The GetCount method
+        // Purpose: returns Count and increments it once each time.
+        // Parameters: none
+        // Returns: double count
+        public int GetCount()
+        {
+            return count++;
+        }
+        // The GetVoltage method
+        // Purpose: voltageArray
+        // Parameters: none
+        // Returns: int i
+        public double GetVoltage(int i)
+        {
+            return voltageArray[i];
+        }
+        // The GetPower method
+        // Purpose: returns powerArray
+        // Parameters: none
+        // Returns: int i
+        public double GetPower(int i)
+        {
+            return powerArray[i];
+        }
+        // The Result method
+        // Purpose: returns Count and increments it once each time.
+        // Parameters: none
+        // Returns: int i
+        public string Result(int i)
+        {
+            // string for holding the result
+            string result;
+            // if powerarray fails to pass
+            if (powerArray[i] < powerRating)
+                // diasplay "no"
+                result = "no";
+            // else return "yes"
+            else result = "yes";
+            return result;
+        }
+    }
+}
